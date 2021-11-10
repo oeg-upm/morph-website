8cf03ca6f0a8f48ffaec0ba2bc650d3842f4b1ff
w0xter <lpozogilo@gmail.com>
2021-11-10 17:28:02 +0100

    Minor change

--- obda/generateRdf.sh
+++ obda/generateRdf.sh
@@ -3,7 +3,7 @@ path=$(pwd)
 yarrrml-parser -i ${path}/mapping.working.yaml -o ${path}/mapping.ttl
 docker rm -f rdfizerContainer
 sleep 5
-docker run --name rdfizerContainer -d -p 4000:4000 -v ${path}:/data rdfizer
+docker run --name rdfizerContainer -d -p 4000:4000 -v ${path}:/data w0xter/rdfizer
 sleep 5
 curl http://localhost:4000/graph_creation/data/config.ini
 
